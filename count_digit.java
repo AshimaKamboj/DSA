b57410b6a169710de8a683c65dfd4fb163c030c8
AshimaKamboj <ashimakamboj27@gmail.com>
2026-01-31 23:27:09 +0530

    count digits

---
+++ count_digit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class count_digit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count =0;
+        while(n>0){
+        int lastdigit=n%10;
+        count=count+1;
+        n=n/10;
+        }
+        System.out.print(count);
+    }
+}
