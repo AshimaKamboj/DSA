b62264779f461d28a8485761032e7f4be6c2cdd3
AshimaKamboj <ashimakamboj27@gmail.com>
2026-02-01 18:28:06 +0530

    check prime

---
+++ check_prime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class check_prime{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        int N = sc.nextInt();
+        for(int i=1;i<=N;i++){
+        if(N%i==0)
+        {
+            count++;
+        }
+        }
+        if(count==2){
+            System.out.print("it is prime");
+        }
+        else
+        {
+            System.out.print("it is not prime");
+        }
+    }
+}
