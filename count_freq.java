c988b35d960c36af3604179b262832a5161dc81b
AshimaKamboj <ashimakamboj27@gmail.com>
2026-02-08 17:57:55 +0530

    count freq implemeted properly

--- count_freq.java
+++ count_freq.java
@@ -1,26 +1,25 @@
 import java.util.*;
-public class count_freq {
-    public static  int count_frequency(int[] arr, int target){
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target){
-                count++;
-            }
+public class count_freq{
+    public static int countfreq(int[] arr,int target){
+    int count=0;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==target){
+            count++;
         }
-         return count;
-
+    }
+    return count;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of elements : ");
         int n = sc.nextInt();
         int[] arr = new int[n];
+        System.out.println("Enter elements of array : ");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
         System.out.print("enter target: ");
         int target = sc.nextInt();
-        System.out.print("freq= "+count_frequency(arr,target));
-
+        System.out.print("freq is : "+countfreq(arr,target));
     }
 }
-
